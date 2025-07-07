e74708b92b08041c348a56cb87b428b31f4d7e90
Shahrukh78692S <mdshahrukhsultan@gmail.com>
2025-07-07 21:21:37 +0530

    Hello Class Created

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Github session");
+        System.out.println("This is Shahrukh");
+        System.out.println("Learning how to push changes in Code to Github Repo");
+        System.out.println("Let's See, If this getting commit or not ");
+        System.out.println("Hurray its showing in Github");
     }
 }
